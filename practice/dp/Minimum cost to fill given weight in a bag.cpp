5471a8451564e4c2532e01f3d2a40aeef8b120e0
Avinash Maurya <avinashm14012001@gmail.com>
2021-03-13 13:20:20 +0530

    unbounded

--- practice/dp/Minimum cost to fill given weight in a bag.cpp
+++ practice/dp/Minimum cost to fill given weight in a bag.cpp
@@ -11,12 +11,27 @@ class Solution{
 		
 
 	public:
-	int minimumCost(int cost[], int N, int W) 
+		int minimumCost(int cost[], int N, int W) 
 	{ 
         // Your code goes here
-        int t[N+1][W+1];
+       long long int t[N+1][W+1];
+        for(int i=0;i<N+1;i++){
+            for(int j=0;j<W+1;j++){
+                if(i==0)
+                    t[i][j]=INT_MAX;
+               else if(j==0)
+                    t[i][j]=0;
+               else if(i<=j&&cost[i-1]!=-1)
+                    t[i][j]=min(cost[i-1]+t[i][j-i],t[i-1][j]);
+                else
+                    t[i][j]=t[i-1][j];
+            }
+        }
+        if(t[N][W]==INT_MAX)
+            return -1;
+        else
+            return t[N][W];
         
-	} 
 };
 
 
